954f2accf0dad34172cb1ae463b89485943fd237
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-11-20 00:46:34 +0100

    hoverable link title

--- src/EntryCard.py
+++ src/EntryCard.py
@@ -12,12 +12,37 @@ from PyQt6.QtCore import QUrl, pyqtSignal, Qt
 from typing import Optional, List
 from Entry import Entry
 
+
+class ActionText(QLabel):
+    """
+    A QLabel that looks like a hyperlink and can be clicked to perform an action. User can define the action by connecting to the clicked signal.
+    """
+
+    clicked = pyqtSignal()
+
+    def __init__(self, text: str, parent: Optional[QWidget] = None):
+        super().__init__(text, parent)
+        self.setCursor(Qt.CursorShape.PointingHandCursor)
+
+    def enterEvent(self, event):
+        font = self.font()
+        font.setUnderline(True)
+        font.setColor(Qt.GlobalColor.blue)
+        self.setFont(font)
+
+    def leaveEvent(self, event):
+        font = self.font()
+        font.setUnderline(False)
+        self.setFont(font)
+
+    def mousePressEvent(self, event):
+        self.clicked.emit()
+
+
 class EntryCard(QWidget):
     entryClicked = pyqtSignal(object)  # Signal emitted when the card is clicked
-    def __init__(
-        self,
-        entry: Entry
-    ):
+
+    def __init__(self, entry: Entry):
         super().__init__()
 
         # Frame that will have the shadow
@@ -35,14 +60,14 @@ class EntryCard(QWidget):
 
         layout = QVBoxLayout(self)
 
-        title_label = QLabel(entry.title)
+        title_label = ActionText(entry.title)
         title_label.setWordWrap(True)
         title_label.setStyleSheet("font-size: 18px; font-weight: bold;")
+        title_label.clicked.connect(lambda: self.entryClicked.emit(self.entry))
 
         authors_label = QLabel(entry.authors)
 
         tag_layout = QHBoxLayout()
-
 
         for category in entry.categories:
             tag_label = QLabel(category)
@@ -57,7 +82,9 @@ class EntryCard(QWidget):
 
         tag_layout.addStretch()
 
-        authors_label.setStyleSheet("font-size: 13px; font-style: oblique; color: #555;")
+        authors_label.setStyleSheet(
+            "font-size: 13px; font-style: oblique; color: #555;"
+        )
 
         date_label = QLabel(f"Published: {entry.published}")
         date_label.setStyleSheet("font-size: 12px; color: #bbb;")
@@ -67,7 +94,9 @@ class EntryCard(QWidget):
         web_btn = QPushButton("Arxiv Page")
 
         link = entry.link
-        pdf_btn.clicked.connect(lambda: QDesktopServices.openUrl(QUrl(link.replace("abs", "pdf"))))
+        pdf_btn.clicked.connect(
+            lambda: QDesktopServices.openUrl(QUrl(link.replace("abs", "pdf")))
+        )
         web_btn.clicked.connect(lambda: QDesktopServices.openUrl(QUrl(link)))
 
         btn_row.addWidget(pdf_btn)
@@ -88,17 +117,16 @@ class EntryCard(QWidget):
         # shadow_effect = QGraphicsDropShadowEffect()
         # shadow_frame.setGraphicsEffect(shadow_effect)
 
-
     # Mouse hover show pointer cursor
-    def enterEvent(self, event):
-        self.setCursor(Qt.CursorShape.PointingHandCursor)
-
-    def leaveEvent(self, event):
-        self.setCursor(Qt.CursorShape.ArrowCursor)
+    # def enterEvent(self, event):
+    #     self.setCursor(Qt.CursorShape.PointingHandCursor)
+    #
+    # def leaveEvent(self, event):
+    #     self.setCursor(Qt.CursorShape.ArrowCursor)
 
     def entry(self) -> Entry:
         return self.entry
 
     # Handle click event and send signal called entryClicked
-    def mousePressEvent(self, event):
-        self.entryClicked.emit(self.entry)
+    # def mousePressEvent(self, event):
+    #     self.entryClicked.emit(self.entry)
