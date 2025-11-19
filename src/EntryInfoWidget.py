4310f2947e58989c8c9fcca059e51f34dce91cc1
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-11-19 19:26:56 +0100

    enable sorting by date,author and title

--- src/EntryInfoWidget.py
+++ src/EntryInfoWidget.py
@@ -1,6 +1,6 @@
 # Entry info widget which shows detailed information like title, abstract about a selected entry.
 
-from PyQt6.QtWidgets import (QWidget, QVBoxLayout, QLabel, QTextEdit, QPushButton)
+from PyQt6.QtWidgets import (QWidget, QVBoxLayout, QLabel, QTextEdit, QPushButton, QHBoxLayout)
 from PyQt6.QtCore import pyqtSignal, Qt
 from PyQt6.QtGui import QFont
 from Entry import Entry
@@ -22,13 +22,31 @@ class EntryInfoWidget(QWidget):
         self.authors_label.setFont(QFont("Arial", 12, QFont.Weight.Normal))
         self.layout.addWidget(self.authors_label)
 
-        self.abstract_text = QTextEdit()
-        self.abstract_text.setReadOnly(True)
+        self.abstract_text = QLabel()
+        self.abstract_text.setWordWrap(True)
+        self.abstract_text.setTextInteractionFlags(Qt.TextInteractionFlag.TextSelectableByMouse)
+        # self.abstract_text.setReadOnly(True)
         self.layout.addWidget(self.abstract_text)
+        self.layout.addStretch()
+
+        self.btn_layout = QHBoxLayout()
 
         self.back_button = QPushButton("Back")
         self.back_button.clicked.connect(self.on_back_clicked)
-        self.layout.addWidget(self.back_button)
+        self.btn_layout.addWidget(self.back_button)
+
+        self.btn_layout.addStretch()
+        # Add pdf button and website button
+
+        self.pdf_button = QPushButton("Open PDF")
+        self.btn_layout.addWidget(self.pdf_button)
+
+        self.website_button = QPushButton("Open Website")
+        self.btn_layout.addWidget(self.website_button)
+
+
+        self.layout.addLayout(self.btn_layout)
+
 
     def on_back_clicked(self):
         self.backClicked.emit()
@@ -36,5 +54,5 @@ class EntryInfoWidget(QWidget):
     def setEntryInfo(self, entry: Entry):
         self.title_label.setText(entry.title)
         self.authors_label.setText(entry.authors)
-        self.abstract_text.setPlainText(entry.abstract)
+        self.abstract_text.setText(entry.abstract)
 
--- src/PaperWatch.py
+++ src/PaperWatch.py
@@ -7,30 +7,32 @@ from PyQt6.QtNetwork import QNetworkAccessManager, QNetworkRequest
 from EntryCard import EntryCard
 from EntryInfoWidget import EntryInfoWidget
 from Entry import Entry
+import enum
+from typing import List, overload, Union
 
 from PyQt6.QtWidgets import (
     QMainWindow,
-    QScrollBar,
     QVBoxLayout,
     QScrollArea,
-    QTableWidget,
-    QTableWidgetItem,
     QWidget,
-    QHBoxLayout,
-    QHeaderView,
-    QSizePolicy,
     QFrame,
-    QStackedWidget
+    QStackedWidget,
 )
 
 from Statusbar import Statusbar
 
-MAX_RESULTS_EACH = 10
+MAX_RESULTS_EACH = 30
 SUBJECTS = ["astro-ph.CO", "cs.CV", "cs.LG", "cs.ML"]
 KEYWORDS = ["CNN", "Machine Learning"]
 
 
 class PaperWatchApp(QMainWindow):
+    # Sort entries general function that sorts according to enum SortBy
+    class SortBy(enum.Enum):
+        DATE = 1
+        TITLE = 2
+        AUTHOR = 3
+
     def __init__(self):
         super().__init__()
 
@@ -49,10 +51,14 @@ class PaperWatchApp(QMainWindow):
         self.manager.finished.connect(self.on_page_response)
 
         # If you want to filter by subjects as well, uncomment the following lines
-        subject_filter = "+OR+".join(f"cat:{subject}" for subject in SUBJECTS)
-        self.parameters = f"({parameters})+AND+({subject_filter})"
-        self.method_name = "search_query"
-        self.fetch_papers_async(self.method_name, self.parameters)
+        # subject_filter = "+OR+".join(f"cat:{subject}" for subject in SUBJECTS)
+        for subject in SUBJECTS:
+            subject_filter = f"cat:{subject}"
+            self.parameters = f"({parameters})+AND+({subject_filter})"
+            self.method_name = "search_query"
+            self.fetch_papers_async(
+                self.method_name, self.parameters, max_results=MAX_RESULTS_EACH
+            )
 
     def initUI(self):
         """Initialize the user interface."""
@@ -63,8 +69,26 @@ class PaperWatchApp(QMainWindow):
         self.view_menu = self.menubar.addMenu("View")
         self.help_menu = self.menubar.addMenu("Help")
 
-        self.file_menu.addAction("Refresh", lambda: self.fetch_papers_async(self.method_name, self.parameters))
+        self.file_menu.addAction(
+            "Refresh",
+            lambda: self.fetch_papers_async(self.method_name, self.parameters),
+        )
         self.file_menu.addAction("Exit", self.close)
+
+        self.view_menu.addAction(
+            "Sort by Date",
+            lambda: self.sort_entries(self.SortBy.DATE),
+            )
+
+        self.view_menu.addAction(
+            "Sort by Title",
+            lambda: self.sort_entries(self.SortBy.TITLE),
+            )
+
+        self.view_menu.addAction(
+            "Sort by Author",
+            lambda: self.sort_entries(self.SortBy.AUTHOR),
+            )
 
         self.layout = QVBoxLayout()
         self.statusbar = Statusbar(self)
@@ -89,19 +113,33 @@ class PaperWatchApp(QMainWindow):
         self.stacked_widget = QStackedWidget()
         self.entry_info_widget = EntryInfoWidget()
 
-        self.entry_info_widget.backClicked.connect(lambda: self.stacked_widget.setCurrentWidget(self.scroll_area))
+        self.entry_info_widget.backClicked.connect(
+            lambda: self.stacked_widget.setCurrentWidget(self.scroll_area)
+        )
 
         self.layout.addWidget(self.stacked_widget)
         self.stacked_widget.addWidget(self.scroll_area)
         self.stacked_widget.addWidget(self.entry_info_widget)
         self.layout.addWidget(self.statusbar)
 
-    def showPapers(self, papers: feedparser.FeedParserDict) -> None:
+    def showPapers(self, papers: Union[feedparser.FeedParserDict, List[Entry]], remove_existing_entries = False) -> None:
         """
         Display fetched papers in the UI.
         """
-        self.entries = papers.entries
-        for feed in papers.entries:
+
+        if remove_existing_entries:
+            # Clear the children of scroll_layout
+            for i in reversed(range(self.scroll_layout.count())):
+                widget_to_remove = self.scroll_layout.itemAt(i).widget()
+                self.scroll_layout.removeWidget(widget_to_remove)
+                widget_to_remove.setParent(None)
+
+        if isinstance(papers, list):
+            self.entries = papers
+        elif isinstance(papers, feedparser.FeedParserDict):
+            self.entries = papers.entries
+
+        for feed in self.entries:
             entry: Entry = Entry(feed)
 
             if entry.primary_category not in SUBJECTS:
@@ -111,14 +149,16 @@ class PaperWatchApp(QMainWindow):
             card.entryClicked.connect(self.on_entry_clicked)
             self.scroll_layout.addWidget(card)
 
-    def fetch_papers_async(self, method_name: str, parameters: str):
+    def fetch_papers_async(
+        self, method_name: str, parameters: str, max_results: int = MAX_RESULTS_EACH
+    ):
         """
         Fetch papers asynchronously from arXiv API.
         """
         self.statusbar.set_message("Loading papers...", 0)
-        url_template = "http://export.arxiv.org/api/query?{method_name}={parameters}&start=0&sortBy=submittedDate&sortOrder=descending"
+        url_template = "http://export.arxiv.org/api/query?{method_name}={parameters}&start=0&max_results={max_results}&sortBy=submittedDate&sortOrder=descending"
 
-        url = f"{url_template.format(method_name=method_name, parameters=parameters)}"
+        url = f"{url_template.format(method_name=method_name, parameters=parameters, max_results=max_results)}"
 
         request = QNetworkRequest(QUrl(url))
         request.setAttribute(QNetworkRequest.Attribute.RedirectPolicyAttribute, True)
@@ -132,17 +172,29 @@ class PaperWatchApp(QMainWindow):
         data = reply.readAll().data()
         feed = feedparser.parse(data)
 
-        # Clear the children of scroll_layout
-        for i in reversed(range(self.scroll_layout.count())):
-            widget_to_remove = self.scroll_layout.itemAt(i).widget()
-            self.scroll_layout.removeWidget(widget_to_remove)
-            widget_to_remove.setParent(None)
-
         self.numPapers = len(feed.entries)
         self.statusbar.set_papers_count(self.numPapers)
-        self.showPapers(feed)
+        self.showPapers(feed, remove_existing_entries=True)
         self.statusbar.set_message("Papers loaded successfully.", 3000)
 
     def on_entry_clicked(self, entry: Entry):
         self.stacked_widget.setCurrentWidget(self.entry_info_widget)
         self.entry_info_widget.setEntryInfo(entry)
+
+    # self.entries is an instance of Entry list
+    def sort_entries(self, sort_by: SortBy):
+        if self.entries is None:
+            return
+
+        if sort_by == self.SortBy.DATE:
+            sorted_entries = sorted(
+                self.entries, key=lambda e: e.published_parsed, reverse=True
+            )
+        elif sort_by == self.SortBy.TITLE:
+            sorted_entries = sorted(self.entries, key=lambda e: e.title.lower())
+        elif sort_by == self.SortBy.AUTHOR:
+            sorted_entries = sorted(self.entries, key=lambda e: e.authors[0]["name"].lower())
+        else:
+            return
+
+        self.showPapers(sorted_entries, True)
