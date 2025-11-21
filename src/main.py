bbbaf12aea8f52f86d871277b95f6fcd0f2ceba1
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-11-21 17:41:29 +0100

    Don't load config here

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from PaperWatch import PaperWatchApp
-from Config import load_config
 
 if __name__ == "__main__":
-    config = load_config("config.toml")
     app = QApplication(sys.argv)
-    window = PaperWatchApp(config)
+    window = PaperWatchApp()
     window.show()
     sys.exit(app.exec())
